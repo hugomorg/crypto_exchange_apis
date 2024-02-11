b18b0fb58103b6a3d989c165b2e8707bc24bb9a6
Hugo <hpjm37@gmail.com>
2024-02-11 16:49:51 +0000

    Update version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CryptoExchangeAPIs.MixProject do
   def project do
     [
       app: :crypto_exchange_apis,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
